184b6cd648ff8f5e0728c84ac74e87fb8420b3c7
전민수 <79839332+alstntjddms@users.noreply.github.com>
2023-06-20 15:58:56 +0900

    api서버 common모듈 일부 의존성 제외처리

--- soso/api/src/main/java/com/api/ApiApplication.java
+++ soso/api/src/main/java/com/api/ApiApplication.java
@@ -4,13 +4,15 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.method.HandlerTypePredicate;
 import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-@ComponentScan(value={"com.common", "com.api"})
+@ComponentScan(basePackages = {"com.common", "com.api"},
+        excludeFilters = @ComponentScan.Filter(type = FilterType.REGEX, pattern = "com.common.db.*"))
 public class ApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(ApiApplication.class, args);
