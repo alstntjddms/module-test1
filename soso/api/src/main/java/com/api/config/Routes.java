66458093ad4db8fc7827901500cb24a2a7d7afcf
전민수 <79839332+alstntjddms@users.noreply.github.com>
2023-07-21 15:21:49 +0900

    spring-doc 추가

--- soso/api/src/main/java/com/api/config/Routes.java
+++ soso/api/src/main/java/com/api/config/Routes.java
@@ -1,7 +1,6 @@
 package com.api.config;
 
 import com.common.url.URL;
-import com.api.config.CustomAuthFilter;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -11,14 +10,20 @@ import org.springframework.context.annotation.Configuration;
 public class Routes {
     @Bean
     public RouteLocator routeLocator(RouteLocatorBuilder builder, CustomAuthFilter customAuthFilter) {
+
         return builder
                 .routes()
+                // 카카오 서버 라우팅
                 .route("kakao_route", p -> p.path("/api/kakao/**")
                         .filters(f -> f.filter(customAuthFilter.apply(new CustomAuthFilter.Config())))
                         .uri(URL.KAKAO_SERVER))
+
+                // 로그인 서버 라우팅
                 .route("login_route", p -> p.path("/api/login/**")
                         .filters(f -> f.filter(customAuthFilter.apply(new CustomAuthFilter.Config())))
                         .uri(URL.AUTH_SERVER))
+
+                // 로그 서버 라우팅
                 .route("log_route", p -> p.path("/api/log/**")
 //                        .filters(f -> f.filter(customAuthFilter.apply(new CustomAuthFilter.Config())))
                         .uri(URL.LOG_SERVER))
--- soso/kakao/src/main/java/com/kakao/controller/KakaoController.java
+++ soso/kakao/src/main/java/com/kakao/controller/KakaoController.java
@@ -11,11 +11,18 @@ import com.kakao.service.itf.KakaoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.bind.annotation.*;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.http.HttpRequest;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-
+import com.microsoft.aad.msal4j.*;
+import java.util.Collections;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
 
 @RestController
 public class KakaoController {
@@ -31,6 +38,43 @@ public class KakaoController {
     @Autowired
     KakaoService kakaoService;
 
+    @GetMapping("/test")
+    public ResponseEntity test() throws Exception {
+        try{
+            log.info("kakao", "Start");
+            System.out.println("getAccessToken = " + getAccessToken());
+            return new ResponseEntity(aes256.encrypt(common.com()), HttpStatus.OK);
+        }catch (Exception e){
+            log.warn("kakao", e.getMessage());
+            return new ResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+
+    @Scheduled
+    public String getAccessToken() throws MalformedURLException, ExecutionException, InterruptedException {
+        IClientCredential credential = ClientCredentialFactory.createFromSecret("oLc8Q~qtX-ZX6my.Gd7z3RM3bMhQQ91J5EaPobyu");
+        ConfidentialClientApplication application = ConfidentialClientApplication.builder("15203123-534e-4f12-b466-f92c5ba7fdf1", credential)
+                .authority("https://login.microsoftonline.com/6369db95-476d-4bbe-8b2b-f6f3926531fb")
+                .build();
+
+        ClientCredentialParameters parameters = ClientCredentialParameters.builder(Collections.singleton("https://graph.microsoft.com/.default"))
+                .build();
+
+        CompletableFuture<IAuthenticationResult> future = application.acquireToken(parameters);
+        IAuthenticationResult tokenResult = future.join();
+
+        IAuthenticationResult result = future.get();
+
+        java.net.http.HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(
+                        "https://graph.microsoft.com/v1.0/applications/" + "81ec712d-7970-4663-85ad-26798417e992"))
+                .header("Authorization", "Bearer " + tokenResult.accessToken())
+                .GET()
+                .build();
+
+        return tokenResult.accessToken();
+    }
     @GetMapping("/kakao")
     public ResponseEntity kakao() throws Exception {
         try{
